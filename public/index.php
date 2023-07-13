eae42d7166da25833da08bf81c1d63e25bd32c42
Richi RM <richiruizmartinez@gmail.com>
2023-07-13 20:22:37 +0200

    Worked For: index.php

--- public/index.php
+++ public/index.php
@@ -11,7 +11,7 @@
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <link rel="icon" href="images/favicon.png" type="image/png" />
-      <title>OneDevs</title>
+      <title>OneDevs - Developing Digital Solutions</title>
       <link rel="stylesheet" href="css/home.min.css">
    </head>
 
@@ -22,6 +22,7 @@
             <ul>
                <li><a href="#quienessomos">Quiénes somos</a></li>
                <li><a href="#quehacemos">Qué hacemos</a></li>
+               <li><a href="#trabajadopara">Hemos trabajado para</a></li>
                <li><a href="#comonoshasconocido">¿Cómo nos has conocido?</a></li>
                <li><a href="#contactaconnosotros">Contacta con nosotros</a></li>
             </ul>
@@ -41,11 +42,11 @@
          <section>
             <h1>Quiénes somos</h1>
             <p>
-               Somos un grupo de profesionales apasionados de la tecnología y el
-               desarrollo web.<br />
+               Somos un grupo de <b>profesionales apasionados de la
+               tecnología</b> y el desarrollo web.<br />
                <br />
-               ¿Tu negocio necesita una web nueva o mantener la actual? ¡Cuenta
-               con nosotros!
+               ¿Tu negocio / empresa / organización necesita una nueva web o
+               mantener la actual? <b>¡Cuenta con nosotros!</b>
             </p>
          </section>
 
@@ -53,13 +54,61 @@
          <section>
             <h1>Qué hacemos</h1>
             <p>
-               Desarrollamos y mantenemos aplicaciones web. Estamos
+               <b>Desarrollamos y mantenemos aplicaciones web.</b> Estamos
                especializados en la creación de sitios web mediante PHP y
                MariaDB.<br />
                <br />
                Gestionamos desde la toma de requisitos hasta la entrega final
                del proyecto y su mantenimiento.
             </p>
+         </section>
+
+         <a name="trabajadopara"></a>
+         <section>
+            <h1>Hemos trabajado para</h1>
+            <div>
+               <div class="workedfor">
+                  <div class="logo"><img src="images/logo-ccep-w500-h300.png" width="300" alt="Coca-Cola Europacific Partners"></div>
+                  <div class="text">
+                     Desarrollo de webs públicas para
+                     <u>promociones comerciales de bebidas energéticas</u>.
+                  </div>
+               </div>
+            </div>
+            <div>
+               <div class="workedfor">
+                  <div class="logo"><img src="images/logo-bbva-w500-h300.png" width="300" alt="BBVA"></div>
+                  <div class="text">
+                     Desarrollo de <u>aplicación interna en WordPress</u>, para
+                     el Banco.
+                  </div>
+               </div>
+            </div>
+            <div>
+               <div class="workedfor">
+                  <div class="logo"><img src="images/logo-mapfre-w500-h300.png" width="300" alt="Mafre"></div>
+                  <div class="text">
+                     Desarrollo de <u>evolutivos para webs internas varias</u>.
+                  </div>
+               </div>
+            </div>
+            <div>
+               <div class="workedfor">
+                  <div class="logo"><img src="images/logo-aecc-w500-h300.png" width="300" alt="AECC"></div>
+                  <div class="text">
+                     <u>Mantenimiento de su web pública principal</u>.
+                  </div>
+               </div>
+            </div>
+            <div>
+               <div class="workedfor">
+                  <div class="logo"><img src="images/logo-aldeasinfantiles-w500-h300.png" width="300" alt="Aldeas Infantiles"></div>
+                  <div class="text">
+                     Desarrollo con Symfony y mantenimiento de <u>web de gestión
+                     de personal de la Empresa</u>.
+                  </div>
+               </div>
+            </div>
          </section>
 
          <a name="comonoshasconocido"></a>
@@ -82,8 +131,9 @@
          <section>
             <h1>Contacta con nosotros</h1>
             <p>
-               Déjanos tu dirección de correo electrónico o un teléfono de
-               contacto y te contactaremos lo antes posible.
+               ¿Tienes alguna necesidad digital? Déjanos tu dirección de correo
+               electrónico o un teléfono de contacto y te contactaremos lo antes
+               posible.
             </p>
             <div>
                <div class="w250">
