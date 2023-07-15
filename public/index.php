45761977856197dbbf97fff89dc15c81434589fd
Richi RM <richiruizmartinez@gmail.com>
2023-07-15 13:10:15 +0200

    Refactory styles: "Mobile first"

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@
 ?>
 <!DOCTYPE html>
 
-<html lang="en">
+<html lang="es">
 
    <head>
       <meta charset="UTF-8">
@@ -19,42 +19,28 @@
 
       <header>
 
-         <div>
-
-            <div>
-               <a href="#inicio">Inicio</a>
-            </div>
-
-            <div>
-               <a href="#quienessomos">Quiénes somos</a>
-            </div>
-
-            <div>
-               <a href="#quehacemos">Qué hacemos</a>
-            </div>
-
-            <div>
-               <a href="#trabajadopara">Hemos trabajado para</a>
-            </div>
-
-            <div>
-               <a href="#comonoshasconocido">¿Cómo nos has conocido?</a>
-            </div>
-
-            <div>
-               <a href="#contactaconnosotros">Contacta con nosotros</a>
-            </div>
-
-         </div>
+         <nav class="menu">
+            <ul>
+               <li><a href="#inicio">Inicio</a></li>
+               <li><a href="#quienessomos">Quiénes somos</a></li>
+               <li><a href="#quehacemos">Qué hacemos</a></li>
+               <li><a href="#trabajadopara">Hemos trabajado para</a></li>
+               <li><a href="#comonoshasconocido">¿Cómo nos has conocido?</a></li>
+               <li><a href="#contactaconnosotros">Contacta con nosotros</a></li>
+            </ul>
+         </nav>
 
       </header>
 
       <main>
 
          <a class="anchor" name="inicio"></a>
-         <section class="logo">
-            <div class="logo">
-               <img class="logo" src="images/logo-onedevs-w300-h315.png" width="250" alt="OneDevs Logo" />
+         <section>
+            <h1>
+               &nbsp;
+            </h1>
+            <div>
+               <img class="logo-onedevs" src="images/logo-onedevs-w300-h315.png" width="250" alt="OneDevs Logo" />
             </div>
          </section>
 
@@ -79,8 +65,8 @@
             </h1>
             <p>
                <b>Desarrollamos y mantenemos aplicaciones web</b>. Estamos
-               especializados en la creación de sitios web mediante PHP y
-               MariaDB.<br />
+               especializados en la creación de sitios web mediante PHP,
+               MariaDB y JavaScript.<br />
                <br />
                Gestionamos <b>desde la toma de requisitos hasta la entrega final
                del proyecto</b> y su mantenimiento.
@@ -90,46 +76,54 @@
          <a class="anchor" name="trabajadopara"></a>
          <section>
             <h1>
-               Hemos trabajado para
+               Hemos trabajado para :
             </h1>
-            <div>
-               <div class="workedfor">
-                  <div class="logo"><img src="images/logo-ccep-w500-h300.png" width="300" alt="Coca-Cola Europacific Partners"></div>
-                  <div class="text">
-                     Desarrollo de webs públicas para <u>promociones comerciales
-                     de bebidas energéticas</u>.
-                  </div>
+            <div class="worked-for">
+               <div class="logo-company">
+                  <img src="images/logo-ccep-w500-h300.png" width="300" alt="Coca-Cola Europacific Partners">
+               </div>
+               <div class="text-company">
+                  Desarrollo de webs públicas para <u>promociones comerciales
+                  de bebidas energéticas</u>.
                </div>
             </div>
             <div>
-               <div class="workedfor">
-                  <div class="logo"><img src="images/logo-bbva-w500-h300.png" width="300" alt="BBVA"></div>
-                  <div class="text">
+               <div class="worked-for">
+                  <div class="logo-company">
+                     <img src="images/logo-bbva-w500-h300.png" width="300" alt="BBVA">
+                  </div>
+                  <div class="text-company">
                      Desarrollo de <u>aplicación interna en WordPress</u>, para
                      el Banco.
                   </div>
                </div>
             </div>
             <div>
-               <div class="workedfor">
-                  <div class="logo"><img src="images/logo-mapfre-w500-h300.png" width="300" alt="Mafre"></div>
-                  <div class="text">
+               <div class="worked-for">
+                  <div class="logo-company">
+                     <img src="images/logo-mapfre-w500-h300.png" width="300" alt="Mafre">
+                  </div>
+                  <div class="text-company">
                      Desarrollo de <u>evolutivos para webs internas</u>.
                   </div>
                </div>
             </div>
             <div>
-               <div class="workedfor">
-                  <div class="logo"><img src="images/logo-aecc-w500-h300.png" width="300" alt="AECC"></div>
-                  <div class="text">
+               <div class="worked-for">
+                  <div class="logo-company">
+                     <img src="images/logo-aecc-w500-h300.png" width="300" alt="AECC">
+                  </div>
+                  <div class="text-company">
                      <u>Mantenimiento de su web pública principal</u>.
                   </div>
                </div>
             </div>
             <div>
-               <div class="workedfor">
-                  <div class="logo"><img src="images/logo-aldeasinfantiles-w500-h300.png" width="300" alt="Aldeas Infantiles"></div>
-                  <div class="text">
+               <div class="worked-for">
+                  <div class="logo-company">
+                     <img src="images/logo-aldeasinfantiles-w500-h300.png" width="300" alt="Aldeas Infantiles">
+                  </div>
+                  <div class="text-company">
                      Desarrollo con Symfony y mantenimiento de <u>web de gestión
                      del personal de la Empresa</u>.
                   </div>
@@ -139,18 +133,18 @@
 
          <a class="anchor" name="comonoshasconocido"></a>
          <section>
-            <h1>¿Cómo nos has conocido?</h1>
-            <div>
-               <div class="w260">
-                  <ul>
-                     <li><input type="radio" id="channel1" name="channel" onchange="how_did_you_meet_us();">&nbsp;&nbsp;Por un conocido/a o amigo/a</li>
-                     <li><input type="radio" id="channel2" name="channel" onchange="how_did_you_meet_us();">&nbsp;&nbsp;Publicidad en Internet</li>
-                     <li><input type="radio" id="channel3" name="channel" onchange="how_did_you_meet_us();">&nbsp;&nbsp;Publicidad impresa</li>
-                     <li><input type="radio" id="channel4" name="channel" onchange="how_did_you_meet_us();">&nbsp;&nbsp;Otros</li>
-                  </ul>
-               </div>
-               <div id="emoji_1">&nbsp;</div>
+            <h1>
+               ¿Cómo nos has conocido?
+            </h1>
+            <div class="how-did-you-meet-us">
+               <ul>
+                  <li><input type="radio" id="channel1" name="channel" onchange="how_did_you_meet_us();">&nbsp;&nbsp;Por un conocido/a o amigo/a</li>
+                  <li><input type="radio" id="channel2" name="channel" onchange="how_did_you_meet_us();">&nbsp;&nbsp;Publicidad en Internet</li>
+                  <li><input type="radio" id="channel3" name="channel" onchange="how_did_you_meet_us();">&nbsp;&nbsp;Publicidad impresa</li>
+                  <li><input type="radio" id="channel4" name="channel" onchange="how_did_you_meet_us();">&nbsp;&nbsp;Otros</li>
+               </ul>
             </div>
+            <div id="emoji_1">&nbsp;</div>
          </section>
 
          <a class="anchor" name="contactaconnosotros"></a>
@@ -163,25 +157,26 @@
                electrónico y te llamaremos lo antes posible.<br />
                <br />
             </p>
-            <div>
-               <div class="w250">
-                  <form class="contact">
-                     <input type="text" id="email_or_phone" placeholder="Teléfono o email" />
-                     <br />
-                     <br />
-                     <input type="button" id="send_contact_button" value="Enviar" onclick="send_contact();" />
-                  </form>
-               </div>
-               <div id="emoji_2">&nbsp;</div>
+            <div class="contact">
+               <form>
+                  <input type="text" id="email_or_phone" placeholder="Teléfono o email" />
+                  <br />
+                  <br />
+                  <input type="button" id="send_contact_button" value="Enviar" onclick="send_contact();" />
+               </form>
             </div>
+            <div id="emoji_2">&nbsp;</div>
          </section>
 
          <section>
             <h1>Más info sobre OneDevs</h1>
             <p>
-               📍 Calle Conquistador Ordoño Álvarez, 6, 1, 1, 2, 14010, Córdoba, España<br />
+               📍 <u>Dirección</u>:<br />
+               Calle Conquistador Ordoño Álvarez,<br />
+               6, 1, 1, 2, 14010, Córdoba, España.<br />
                <br />
-               📱 +34 744 653 072
+               📱 <u>Teléfono móvil</u>:<br />
+               +34 744 653 072
             </p>
          </section>
 
@@ -195,11 +190,13 @@
       </main>
 
       <footer>
+
          <p>
             &copy; 2023 OneDevs&nbsp;&nbsp;&nbsp;
             Política de Privacidad&nbsp;&nbsp;&nbsp;
             Política de Cookies&nbsp;&nbsp;&nbsp;
          </p>
+
       </footer>
 
       <script src="js/home.js"></script>
